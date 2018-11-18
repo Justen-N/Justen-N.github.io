bb349684c87701790b02e9e362981bbf5a9dcea6
Justen Neeley <losthalflingrogue@gmail.com>
2018-11-17 20:11:45 -0700

    finish activity

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -5,7 +5,9 @@ console.log('My javascript is being read.')
 // test variables ( currently  in production)
 const temp = 31;
 const speed = 5;
+const direction = "NE";
 buildWC(speed,temp);
+windDial(direction);
 
 //build and display the "Feels Like" section
 function buildWC(speed,temp){
@@ -23,3 +25,55 @@ function buildWC(speed,temp){
     wc = 'Feels like ' + wc + '&deg;F';
     feelTemp.innerHTML = wc;
 }
+
+// Wind Dial Function
+function windDial(direction){
+    // Get the container
+    const dial = document.getElementById("pointer");
+    console.log(direction);
+    // Determine the dial class
+    switch (direction){
+     case "North":
+     case "N":
+      dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+      break;
+     case "NE":
+     case "NNE":
+     case "ENE":
+      dial.setAttribute("class", "ne");
+      break;
+     case "NW":
+     case "NNW":
+     case "WNW":
+      dial.setAttribute("class", "nw");
+      break;
+     case "South":
+     case "S":
+      dial.setAttribute("class", "s");
+      break;
+     case "SE":
+     case "SSE":
+     case "ESE":
+      dial.setAttribute("class", "se");
+      break;
+     case "SW":
+     case "SSW":
+     case "WSW":
+      dial.setAttribute("class", "sw");
+      break;
+     case "East":
+     case "E":
+      dial.setAttribute("class", "e");
+      break;
+     case "West":
+     case "W":
+      dial.setAttribute("class", "w");
+      break;
+    }
+   }
+
+   // finds the current condition for background display
+
+   function getCondition(currentCondition){
+    
+   }
